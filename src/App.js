ed970681b373b19f796e673d1b2942252f126b32
Kieran Caraway <kierancaraway20@yahoo.com>
2024-10-24 15:00:56 -0500

    latest update
    practice

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from './components/Header';
+import Body from './components/Body';
+import "./style.css"
+
+
+function App() {
+    return(
+        <div className='container'>
+            <Header />
+            <Body />
+        </div>
+    )
+}
+
+
+
+export default App;
---
+++ src/components/Body.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+
+function Body() {
+
+
+    return(
+        <div className='row'>
+            <div className="col-12">
+                <ol>
+                    <li>Man of Steel</li>
+                    <li>Michael Myers</li>
+                    <li>The Fast and the Furious</li>
+                    <li>I am legend</li>
+                    <li>The road to ElDorado</li>
+                </ol>
+            </div>
+        </div>
+    )
+}
+
+export default Body;
